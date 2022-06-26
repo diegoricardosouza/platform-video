d68095c149312c5a572c8c61973ebe9b963dbf7e
Diego Ricardo <diegoricardoweb@gmail.com>
2022-06-25 22:06:15 -0300

    Finalizado a pagina event

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import Event from "./pages/Event"
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/event" element={<Event />} />
+        </Routes>
+    )
+}
+
+export default Router
